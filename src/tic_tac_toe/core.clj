ec0f675fa3b63731a051c396fa4fa73d85241356
James Craig Burley <james-github@burleyarch.com>
2017-11-15 17:14:59 -0500

    Get some basic argument processing working.

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,29 +1,64 @@
 (ns tic-tac-toe.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as string])
   (:gen-class))
 
-(comment (def cli-options
-  (comment
-    ;; An option with a required argument
-    [["-p" "--port PORT" "Port number"
-      :default 80
-      :parse-fn #(Integer/parseInt %)
-      :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-     ;; A non-idempotent option
-     ["-v" nil "Verbosity level"
-      :id :verbosity
-      :default 0
-      :assoc-fn (fn [m k _] (update-in m [k] inc))])
-    ;; A boolean option defaulting to nil
-    ["-h" "--help"]])
+(def cli-options
+  ;; An option with a required argument
+  [["-v" "--verbose" "Verbosity level"
+    :id :verbosity
+    :default 0
+    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
 
-; My original hacky attempt to parse options:
-(defn parse-opts
-  ""
-  [fn args]
-  (if (some (partial = "--help") args)
-    (usage)
-    (fn args)))
+(defn usage [options-summary]
+  (->> ["This is a primitive tic-tac-toe program. There are many like it, but this one is mine."
+        ""
+        "Usage: tic-tac-toe [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "  start    Start a new game"
+        ""
+        "Please refer to the source code for more information."]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn validate-args
+  "Validate command line arguments. Either return a map indicating the program
+  should exit (with a error message, and optional ok status), or a map
+  indicating the action the program should take and the options provided."
+  [args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) ; help => exit OK with usage summary
+      {:exit-message (usage summary) :ok? true}
+      errors ; errors => exit with description of errors
+      {:exit-message (error-msg errors)}
+      ;; custom validation on arguments
+      (and (= 1 (count arguments))
+           (#{"start"} (first arguments)))
+      {:action (first arguments) :options options}
+      :else ; failed custom validation => exit with usage summary
+      {:exit-message (usage summary)})))
+
+(defn exit [status msg]
+  (println msg)
+  {:exit status})
+
+;; My original hacky attempt to parse options:
+;;(defn parse-opts
+;;  ""
+;;  [fn args]
+;;  (if (some (partial = "--help") args)  <== TODO: learn the Clojure way to do this
+;;    (usage)
+;;    (fn args)))
 
 (defn- board-indices
   "Return indices for a board"
@@ -34,13 +69,13 @@
 
 (defprotocol Player
   "Define interface for fns that implement (or talk with) a player"
-  ; Choose next move for the player, based on Game.
+  ;; Choose next move for the player, based on Game.
   (choose [this])
-  ; Player should be advised as to valid moves.
+  ;; Player should be advised as to valid moves.
   (wants-valid-moves? [this])
-  ; Report state of Game to player. 'why' is :move, :retry, :win,
-  ; :lose, or :draw; 'what' is nil or { <player-kw> <move> }; 'valid'
-  ; is optional set of valid moves (1-9), or nil if not provided.
+  ;; Report state of Game to player. 'why' is :move, :retry, :win,
+  ;; :lose, or :draw; 'what' is nil or { <player-kw> <move> }; 'valid'
+  ;; is optional set of valid moves (1-9), or nil if not provided.
   (report [this why what valid]))
 
 (defn valid-move?
@@ -49,12 +84,12 @@
   (or (= move :resign)
       (and
        (integer? move)
-       (nil? (get board move true)))))  ; Cell occupied?
+       (nil? (get board move true)))))  ;; Cell occupied?
 
 (defn valid-moves
   "Returns set of valid moves for the player to move next"
   [board]
-  ; TODO: simplify this filter, if possible
+  ;; TODO: simplify this filter, if possible
   (filter (fn [n] (nil? (get board n))) (board-indices)))
 
 (defn read-move-interactive
@@ -72,23 +107,23 @@
       :else nil)))
 
 (comment (do
-     (println "Invalid input; please enter valid move or 'resign'.")
-     (flush)
-     (recur))
+           (println "Invalid input; please enter valid move or 'resign'.")
+           (flush)
+           (recur)))
 
 
-; determine-move-randomly picks a random valid move, but never :resign.
+;; determine-move-randomly picks a random valid move, but never :resign.
 
 
-; next-valid-move takes a fn that determines or reads a move, a fn
-; that validates the move, a set of valid board moves for a player,
-; calls the fn repeatedly until a non-nil result is returned that is
-; validated, and ultimately returns the valid move.
+;; next-valid-move takes a fn that determines or reads a move, a fn
+;; that validates the move, a set of valid board moves for a player,
+;; calls the fn repeatedly until a non-nil result is returned that is
+;; validated, and ultimately returns the valid move.
 
 
-; update-game returns a new game based on a existing game plus a move,
-; and returns :draw, an player (:X or :O), or a set of valid next
-; moves and player to make the move.
+;; update-game returns a new game based on a existing game plus a move,
+;; and returns :draw, an player (:X or :O), or a set of valid next
+;; moves and player to make the move.
 
 
 (defn- new-board
@@ -101,16 +136,29 @@
   []
   (Game. nil :X (new-board)))
 
-(defn start-game
+(defn start-game!
   ""
   [args]
   (println "Hello, World! It's me!! And I still don't play tic-tac-toe!!")
-  (do
-    (doall (for [arg args] (println arg)))
-    :success))
+  args)
+
+(defn main
+  "Use this in a REPL -- it won't exit, and it'll parse the args"
+  [args]
+  (comment (start-game! nil))
+  (let [{:keys [action options exit-message ok?]} (validate-args
+                                                   (if (string? args)
+                                                     (string/split args #" ")
+                                                     args))]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (case action
+        "start"  (start-game! options)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "The formal main entrypoint"
   [& args]
-  (parse-opts args cli-options)
-  (comment (start-game nil))
+  (let [res (main args)]
+    (if-let [rc (:exit res)]
+      (System/exit rc)
+      res)))
