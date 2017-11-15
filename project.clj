db09e3e6c43b366dc2c5eea0ce82702dd3972266
James Craig Burley <james-github@burleyarch.com>
2017-11-15 17:15:36 -0500

    Pull in CLI parser

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jcburley/tic-tac-toe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main tic-tac-toe.core
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/tools.cli "0.3.5"]]
+  :main ^:skip-aot tic-tac-toe.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
