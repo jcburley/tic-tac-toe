baab1a171bbf3df85d34218e69f3d6863d1737aa
James Craig Burley <james-github@burleyarch.com>
2017-11-16 14:12:26 -0500

    More progress

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -163,20 +163,36 @@
   (winner-status player
                  (map #(board-cell board %) triad-positions)))
 
-(defn player-winning-triad-positions
-  ""
+(defn winning-triad-positions-for-player
+  "Return list of winning positions 'owned' by given player"
   [board player]
-  (filter #(first %)
-          (map #(list
-                 (winner-status-for-triad-positions board player %)
-                 %)
-               winning-positions)))
+  (map #(first (rest %))
+       (filter #(first %)
+               (map #(list
+                      (winner-status-for-triad-positions board player %)
+                      %)
+                    winning-positions))))
 
 (defn winners-for-board
   "Return map of players with list of winning positions 'owned' by each player"
   [board]
-  {:X (player-winning-triad-positions board :X)
-   :O (player-winning-triad-positions board :O)})
+  {:X (winning-triad-positions-for-player board :X)
+   :O (winning-triad-positions-for-player board :O)})
+
+(defn game-status
+  "Returns :draw, (:X <list-of-winning-positions>) (meanings :X
+  wins), (:O <list-of-winning-positions>) (:O wins), or nil (game not
+  over)"
+  [board]
+  (let [w (winners-for-board board)]
+       (cond
+         (> (count (:X w))(count (:O w)))
+         (list :X (:X w))
+         (> (count (:O w))(count (:X w)))
+         (list :O (:O w))
+         (some nil? board)
+         nil
+         :else :draw)))
 
 (defn start-game!
   ""
